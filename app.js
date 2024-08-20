f47cfb1866403477cbaf9581acfa5a8ecc627f43
zidan_1013 <80787878+zidan4@users.noreply.github.com>
2024-08-20 22:23:35 +0300

    Update app.js

--- app.js
+++ app.js
@@ -8,6 +8,7 @@ function padRow(rowNumber, rowCount) {
   return " ".repeat(rowCount - rowNumber) + character.repeat(2 * rowNumber - 1) + " ".repeat(rowCount - rowNumber);
 }
 
+
 for (let i = 1; i <= count; i++) {
   if (inverted) {
     rows.unshift(padRow(i, count));
